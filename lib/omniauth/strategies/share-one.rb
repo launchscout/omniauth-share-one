f0df949910c71424a097cf920f5eea3978bff29d
Josh Owens <joshua.owens@gmail.com>
2012-06-26 12:23:12 -0400

    Change up how we geneerate access token requests.

--- lib/omniauth/strategies/share-one.rb
+++ lib/omniauth/strategies/share-one.rb
@@ -5,44 +5,32 @@ module OmniAuth
   module Strategies
     class ShareOne < OmniAuth::Strategies::OAuth
       option :name, 'share-one'
-      option :client_options, {:authorize_path => '/oauth/authorize',
-                               :site => 'https://emery-md.nssecurebanking.org'}
+      option :client_options, {
+        :site => 'https://emery-md.nssecurebanking.org',
+        :authorize_url => '/oauth2/authorize',
+        :token_url => '/oauth2/token'
+      }
 
-      uid { access_token.params[:user_id] }
-
-      info do
-        {
-          :nickname => raw_info['screen_name'],
-          :name => raw_info['name'],
-          :location => raw_info['location'],
-          :image => raw_info['profile_image_url'],
-          :description => raw_info['description'],
-          :urls => {
-            'Website' => raw_info['url'],
-            'ShareOne' => 'http://share-one.com/' + raw_info['screen_name'],
-          }
-        }
+      def authorize_params
+        super.tap do |params|
+          params[:response_type] = "code"
+          params[:client_id] = client.id
+          params[:redirect_uri] ||= callback_url
+        end
       end
-
-      extra do
-        { :raw_info => raw_info }
-      end
-
-      def raw_info
-        @raw_info ||= MultiJson.load(access_token.get('/api/v1/accounts').body)
-      rescue ::Errno::ETIMEDOUT
-        raise ::Timeout::Error
-      end
-
-      alias :old_request_phase :request_phase
 
       def request_phase
-        screen_name = session['omniauth.params'] ? session['omniauth.params']['screen_name'] : nil
-        if screen_name && !screen_name.empty?
-          options[:authorize_params] ||= {}
-          options[:authorize_params].merge!(:force_login => 'true', :screen_name => screen_name)
-        end
-        old_request_phase
+        super
+      end
+
+      def build_access_token
+        token_params = {
+          :code => request.params['code'],
+          :redirect_uri => callback_url,
+          :client_id => client.id,
+          :grant_type => 'authorization_code'
+        }
+        client.get_token(token_params)
       end
     end
   end
