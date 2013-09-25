04647157ad65b10080fc040721a4f74d26e8fd30
Bill Barnett <bill@gaslight.co>
2013-09-25 16:46:17 -0400

    Fix test

--- spec/omniauth/strategies/share_one_spec.rb
+++ spec/omniauth/strategies/share_one_spec.rb
@@ -15,7 +15,7 @@ describe OmniAuth::Strategies::ShareOne do
     end
 
     it 'should have correct authorize url' do
-      subject.options.client_options.authorize_path.should eq('/oauth/authorize')
+      subject.options.client_options.authorize_url.should eq('/oauth/authorize')
     end
   end
 end
